14d048405100a77b7e51b9a12bdddcb14fc6981f
SEOJEONG <plant00park21@gmail.com>
2021-02-28 23:55:42 +0900

    admin_user page set up

--- envs/django-with-react-rev2/accounts/forms.py
+++ envs/django-with-react-rev2/accounts/forms.py
@@ -4,9 +4,23 @@ from django.contrib.auth.forms import (
 )
 from .models import User
 
-# class SignupForm(forms.ModelForm):
-    # class Meta:
+
 class SignupForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].required = True
+        self.fields['first_name'].required = True
+        self.fields['last_name'].required = True
+
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ['username', 'password']
+        fields = ['username', 'email', 'first_name', 'last_name']
+
+    def clean_email(self):
+        email = self.cleaned_data_get('email')
+        if email:
+            qs = User.objects.filter(email=email)
+            if qs.exists():
+                raise forms.ValidationError("이미 등록된 이메일 주소입니다.")
+        return email
+        
