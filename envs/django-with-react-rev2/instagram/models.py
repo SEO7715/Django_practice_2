9d42612ab970ee0ce72d387370f9b88c6013fe5b
SEOJEONG <plant00park21@gmail.com>
2021-03-08 22:58:12 +0900

    post_detail

--- envs/django-with-react-rev2/instagram/models.py
+++ envs/django-with-react-rev2/instagram/models.py
@@ -1,6 +1,7 @@
 import re
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -11,7 +12,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.caption
-    
+ 
     def extract_tag_list(self):
         tag_name_list = re.findall(r"#([a-zA-Z\dㄱ-힣]+)", self.caption)
         tag_list = []
@@ -20,6 +21,9 @@ class Post(models.Model):
             tag_list.append(tag)
         return tag_list
 
+    def get_absolute_url(self):
+        return reverse("instagram:post_detail", args=[self.pk])
+    
 
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
